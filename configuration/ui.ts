c5e8ca37ad58f7431d12c6d6195efc4e2b0b60c7
Aaditya Mulani <mulaniaaditya98@gmail.com>
2025-02-17 15:16:17 -0500

    Changed footer text and chat header to my own
    Original
    [FOOTER TEXT]
    and 
    [CHAT HEADER]

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,8 +1,8 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `[CHAT HEADER]`;
+export const CHAT_HEADER: string = `Aaditya 2.0`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[FOOTER MESSAGE]`;
+export const FOOTER_MESSAGE: string = `Copyright 2025. Aaditya Mulani. Confidential`;
 export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
 export const PAGE_TITLE: string = `[PAGE TITLE]`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
