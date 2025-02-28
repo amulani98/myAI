6ab4e51edcb1c283545b9d4ebca9883157b7b310
Aaditya Mulani <mulaniaaditya98@gmail.com>
2025-02-28 12:49:06 -0500

    Update ui.ts - cricket
    Customized to all cricket related items

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Aaditya 2.0`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const CHAT_HEADER: string = `Cricket Guru`;
+export const MESSAGE_PLACEHOLDER: string = `Chat with me to learn more about cricket`;
 export const FOOTER_MESSAGE: string = `Copyright 2025. Aaditya Mulani. Confidential`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
-export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
+export const CLEAR_BUTTON_TEXT: string = `Reverse Sweep`;
+export const PAGE_TITLE: string = `Cricket Guru`;
+export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant on all things cricket.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
